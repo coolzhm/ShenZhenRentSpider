a3f01e2c0e0c45d750761e78ba5c00853c37e1ac
coolzhm <357270546@qq.com>
2017-10-10 15:38:37 +0800

    # ShenZhenRentSpider python3.5 + scrapy1.4 爬取房天下深圳租房信息 问题点及进展： 1、爬取两个页面就断开 (已修复，能一直往下爬取) 2、之前爬取页面第二个页面之后会爬取不到数据，是因为列表的XPATH首页跟子页面有所不同（已修复） 2017-10-09 3、同2问题点导致后面页面爬取数据为None（已修复） 2017-10-10 3、问题点为爬取网页存储的数据量与核算的数据量不全（待修复）
    输入下列命令执行爬虫，并将爬取数据存入test.csv
    scrapy crawl ftx -o test.csv
    
    输入下列命令执行爬虫则直接爬取数据并存储在FangTianXia.json文件内
    scrapy crawl ftx
    
    功能优化日历
    1、20170922 增加json数据存储
    2、20171009 修复爬取页面下一页之后会爬取不到数据的问题
    3、20171010 修复爬取数据为None问题，Setting中增加自动限速(AutoThrottle)扩展
    
    QQ：357270546

--- ShenZhenRentSpider/pipelines.py
+++ ShenZhenRentSpider/pipelines.py
@@ -9,15 +9,18 @@ import json
 
 class ShenzhenrentspiderPipeline(object):
     def __init__(self):
-        self.file = codecs.open('douban.json', 'w+', encoding='utf-8')
+        self.file = codecs.open('FangTianXia.json', 'w+', encoding='utf-8')
+
 
     def process_item(self, item, spider):
+        # print ('[========= {0} =========]'.format(item['url']))
         line = json.dumps(dict(item), ensure_ascii=False) + "\n"
         self.file.write(line)
         return item
 
     def spider_closed(self, spider):
         self.file.close()
+
 
 # 将爬取数据存入MYSQL
 class SetDataToMySQLPipeline(object):
--- ShenZhenRentSpider/settings.py
+++ ShenZhenRentSpider/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.25
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -65,19 +65,19 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'ShenZhenRentSpider.pipelines.ShenzhenrentspiderPipeline': 300,
+   'ShenZhenRentSpider.pipelines.ShenzhenrentspiderPipeline': 1,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
