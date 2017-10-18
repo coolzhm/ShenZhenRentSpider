ab55dafbdf05c7fd1afb80b1b6a4b00f07f294a4
DaMing <357270546@qq.com>
2017-10-18 22:39:15 +0800

    20171018 增加User-Agent列表

--- ShenZhenRentSpider/spiders/fangtianxia_spider.py
+++ ShenZhenRentSpider/spiders/fangtianxia_spider.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 
 from scrapy.spider import Spider
 import scrapy
@@ -17,6 +18,25 @@ class FangTianXiaSpider(Spider):
     # 数据量计数器
     data_count = 0
     allowed_domains = ['fang.com', 'zu.sz.fang.com', 'zu.sz.fang.com/house/']
+
+    USER_AGENTS = [
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+        "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+        "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+        "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+        "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+        "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+        "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+    ]
 
     # 遍历爬取
     def parse(self, response):
@@ -53,6 +73,7 @@ class FangTianXiaSpider(Spider):
             detail_url = "http://zu.sz.fang.com{0}".format(
                 response.xpath('//*[@class="houseList"]/dl[{0}]/dd/p[1]/a/@href'.format(i)).extract_first())
 
+
             yield scrapy.Request(detail_url, callback=self.parse_rent_info, meta={'item': item})
 
         # 遍历页码的标签，查看是否存在 下一页
@@ -64,7 +85,18 @@ class FangTianXiaSpider(Spider):
                 next_url = response.xpath('//*[@id="rentid_D10_01"]/a[%s]/@href' % j).extract_first()
                 result = u'下一页' in str(value)
                 if (result):
-                    yield scrapy.Request("http://zu.sz.fang.com{0}".format(next_url), callback=self.parse)
+                    headers = {
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                        'Accept-Encoding': 'gzip, deflate',
+                        'Accept-Language': 'zh-CN,zh;q=0.8',
+                        'Cache-Control': 'max-age=0',
+                        'Connection': 'keep-alive',
+                        'User-Agent': random.choice(self.USER_AGENTS)
+                    }
+                    print('当前User-Agent为')
+                    print(headers['User-Agent'])
+
+                    yield scrapy.Request("http://zu.sz.fang.com{0}".format(next_url), callback=self.parse, headers=headers)
 
             print('----------------------- 爬取第【{0}】页面结束 --------------------------'.format(self.page_count))
             print('----------------------- 总计爬取【{0}】数据量 --------------------------'.format(self.data_count))
